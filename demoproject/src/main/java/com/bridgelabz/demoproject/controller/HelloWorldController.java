82a518b823ac449416b17440ac21e828e571d77c
pankaj kore <korep95@gmail.com>
2022-04-11 01:11:05 +0530

    [pankaj]added Project of restAPI messgae file

---
+++ demoproject/src/main/java/com/bridgelabz/demoproject/controller/HelloWorldController.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.demoproject.controller;
+
+
+import com.bridgelabz.demoproject.entity.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+    
+}
---
+++ demoproject/src/test/java/com/bridgelabz/demoproject/MyFirstSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.demoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
