457f7ceb38e7753c760552e6ae4bb87ab97c775d
pankaj kore <korep95@gmail.com>
2022-04-11 01:13:43 +0530

    [pankaj]added request method message

--- demoproject/src/main/java/com/bridgelabz/demoproject/controller/HelloWorldController.java
+++ demoproject/src/main/java/com/bridgelabz/demoproject/controller/HelloWorldController.java
@@ -7,5 +7,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
-    
+
+    //UC1
+    @RequestMapping (value = {"" , "/" , "/get" , "/home"},method = RequestMethod.GET)
+    public String helloWorld(){
+        return "Hello From BridgeLabz";
+    }
 }
